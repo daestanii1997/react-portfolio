79fb8ebefa64befb5196ea460fff9404f1637343
Dae'Stanii <daestanii1997@gmail.com>
2024-01-25 15:02:47 -0700

    edited about me page content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,15 @@ export default function Footer() {
     return (
         <footer>
             <a href='https://github.com/daestanii1997'>
-                <img className='footer-images' src='github-icon.png' style={{borderRadius: '30px'}} ></img>
+                <img className='footer-images' src='github-icon.png' alt='github icon' style={{borderRadius: '30px'}} ></img>
             </a>
 
-            <a href='www.linkedin.com/in/dae-stanii-spackman-280b5a182'>
-                <img className='footer-images' src='linkedin-logo.png' style={{borderRadius: '10px'}} ></img>
+            <a href='https://www.linkedin.com/in/dae-stanii-spackman-280b5a182'>
+                <img className='footer-images' src='linkedin-logo.png' alt='linked in icon' style={{borderRadius: '10px'}} ></img>
             </a>
 
             <a href='https://stackexchange.com/users/29841091/dae'>
-                <img className='footer-images' src='stact-logo.png' style={{borderRadius: '10px'}} ></img>
+                <img className='footer-images' src='stact-logo.png' alt='stack exchange icon' style={{borderRadius: '10px'}} ></img>
             </a>
 
         </footer>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,15 +2,21 @@ export default function About() {
   return (
     <div className="about-container">
       <section className="about-title">
-        <img className="avatar" src="avatar.png"></img>
+        <img className="avatar" src="avatar.png" alt="avatar"></img>
         <h1>About me...</h1>
       </section>
 
       <p className="about-content">
-        I was born and raised in Salt Lake City, and am a nature lover. I just
-        got married and have a sweet puppy named Kai. I do different kinds of
-        art in my free time and love to spend time up in the mountains for
-        inspiration. I love gaming and binging good tv shows.
+        I am a Full Stack Web Developer with extensive knowledge in MERN
+        applications and MySQL data manipulation. I have experience with
+        multiple CSS frameworks that utilize responsive web design. My
+        background in administration allows me to look at each website from a
+        users perspective and focus on the user experience, including
+        accessiblity.
+      </p>
+      <p className="about-content">
+        When I am not at my desk coding, I enjoy painting, exploring different
+        art mediums and spending time with my dog.
       </p>
     </div>
   );
